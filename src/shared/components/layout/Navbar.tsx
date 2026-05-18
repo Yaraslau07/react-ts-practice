ea840d598b56db85e146171c741224c2aeaff75a
Yaraslau <fdyjjpasr@gmail.com>
2026-05-18 15:45:01 +0200

    feat: implemented popups

--- src/shared/components/layout/Navbar.tsx
+++ src/shared/components/layout/Navbar.tsx
@@ -10,6 +10,7 @@ import {
     navigationMenuTriggerStyle,
 } from '@/shared/components/ui/navigation-menu'
 import { ACTION_BUTTONS, NAV_ITEMS } from '@/shared/config/navbarConfig'
+import { usePopup } from '@/shared/hooks/popup/usePopup'
 import { useTheme } from '@/shared/providers/ThemeProvider'
 
 export default function Navbar() {
@@ -17,8 +18,11 @@ export default function Navbar() {
 
     const { theme, setTheme } = useTheme()
 
+    const { showPopup, PopupComponent } = usePopup()
+
     return (
         <nav className="sticky top-0 z-10 flex h-16 w-full items-center justify-between border-b border-slate-200 bg-white px-4 transition-colors md:px-6 dark:border-slate-800 dark:bg-slate-950">
+            {PopupComponent}
             <div className="flex items-center md:hidden">
                 <button
                     onClick={() => setIsNavbarOpened(!isNavbarOpened)}
@@ -45,7 +49,15 @@ export default function Navbar() {
                                             <item.icon size={18} /> {item.label}
                                         </Link>
                                     ) : (
-                                        <button className="flex items-center gap-2 text-slate-500 dark:text-slate-400 dark:hover:text-slate-200">
+                                        <button
+                                            onClick={() =>
+                                                showPopup(
+                                                    'No such page available yet',
+                                                    'error'
+                                                )
+                                            }
+                                            className="flex items-center gap-2 text-slate-500 dark:text-slate-400 dark:hover:text-slate-200"
+                                        >
                                             <item.icon size={18} /> {item.label}
                                         </button>
                                     )}
@@ -68,6 +80,7 @@ export default function Navbar() {
                 {ACTION_BUTTONS.map((btn) => (
                     <button
                         key={btn.id}
+                        onClick={() => showPopup('Not yet available', 'error')}
                         className="hidden rounded-full p-2 text-slate-500 transition hover:bg-slate-100 sm:flex dark:text-slate-400 dark:hover:bg-slate-800"
                     >
                         <btn.icon size={20} />
